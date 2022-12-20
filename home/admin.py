dd03b477ee55e37f122ac1dd6e9795c70011acd0
MSA2001 <msa2001b21@gmail.com>
2022-12-20 19:59:43 +0330

    DONE 1

--- home/admin.py
+++ home/admin.py
@@ -3,7 +3,15 @@ from .models import Category, Drink, About, Contact
 
 # Register your models here.
 
+
+
+@admin.register(Drink)
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'images')
+    list_editable = ('price',)
+    search_fields = ('name', )
+
+
 admin.site.register(Category)
-admin.site.register(Drink)
 admin.site.register(About)
 admin.site.register(Contact)
--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 
 # Create your models here.
 
@@ -17,12 +18,15 @@ class Category(models.Model):
 class Drink(models.Model):
     name = models.CharField(max_length=100)
     category = models.ManyToManyField(Category, related_name='drinks')
-    detail = models.TextField(max_length=200)
+    detail = models.TextField(max_length=500)
     price = models.FloatField()
     image = models.ImageField(upload_to='images/drinks')
 
     def __str__(self):
         return f'{self.name}-- {self.price}$'
+
+    def images(self):
+        return format_html(f"<img src='{self.image.url}' width='30px' height='30px'> ")
 
 
 class About(models.Model):
