86c6c432bfabb41db7fe7b3f9d59178df9656e7f
MSA2001 <msa2001b21@gmail.com>
2022-12-20 19:05:49 +0330

    Contact us

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Drink, About
+from .models import Category, Drink, About, Contact
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Drink)
 admin.site.register(About)
+admin.site.register(Contact)
--- home/models.py
+++ home/models.py
@@ -34,4 +34,12 @@ class About(models.Model):
         return self.title
 
 
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=85)
+    message = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return f'{self.name} : {self.message[:50]}'
+
 
